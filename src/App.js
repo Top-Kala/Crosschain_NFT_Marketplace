5753b4bdcaf88e110d112d26f9ffdb188ecc2a9e
kevinharrisonfree77 <kevinharrisonfree77@gmail.com>
2021-12-21 06:59:57 -0800

    react design initi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.scss';
+import AppRoutes from './AppRoutes';
+import Navbar from './shared/Navbar';
+import Sidebar from './shared/Sidebar';
+import Footer from './shared/Footer';
+import { withTranslation } from "react-i18next";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {}
+  componentDidMount() {
+    this.onRouteChanged();
+  }
+  render () {
+    let navbarComponent = !this.state.isFullPageLayout ? <Navbar/> : '';
+    let sidebarComponent = !this.state.isFullPageLayout ? <Sidebar/> : '';
+    let footerComponent = !this.state.isFullPageLayout ? <Footer/> : '';
+    return (
+      <div className="container-scroller">
+        { sidebarComponent }
+        <div className="container-fluid page-body-wrapper">
+          { navbarComponent }
+          <div className="main-panel">
+            <div className="content-wrapper">
+              <AppRoutes/>
+            </div>
+            { footerComponent }
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.location !== prevProps.location) {
+      this.onRouteChanged();
+    }
+  }
+
+  onRouteChanged() {
+    console.log("ROUTE CHANGED");
+    const { i18n } = this.props;
+    const body = document.querySelector('body');
+    if(this.props.location?.pathname === '/layout/RtlLayout') {
+      body.classList.add('rtl');
+      //i18n.changeLanguage('ar');
+    }
+    else {
+      body.classList.remove('rtl')
+      //i18n.changeLanguage('en');
+    }
+    window.scrollTo(0, 0);
+    const fullPageLayoutRoutes = ['/user-pages/login-1', '/user-pages/login-2', '/user-pages/register-1', '/user-pages/register-2', '/user-pages/lockscreen', '/error-pages/error-404', '/error-pages/error-500', '/general-pages/landing-page'];
+    for ( let i = 0; i < fullPageLayoutRoutes.length; i++ ) {
+      if (this.props.location?.pathname === fullPageLayoutRoutes[i]) {
+        this.setState({
+          isFullPageLayout: true
+        })
+        document.querySelector('.page-body-wrapper').classList.add('full-page-wrapper');
+        break;
+      } else {
+        this.setState({
+          isFullPageLayout: false
+        })
+        document.querySelector('.page-body-wrapper').classList.remove('full-page-wrapper');
+      }
+    }
+  }
+
 }
 
-export default App;
+export default withTranslation()(App);
---
+++ src/shared/Navbar.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Dropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Trans } from 'react-i18next';
+
+class Navbar extends Component {
+  toggleOffcanvas() {
+    document.querySelector('.sidebar-offcanvas').classList.toggle('active');
+  }
+  toggleRightSidebar() {
+    document.querySelector('.right-sidebar').classList.toggle('open');
+  }
+  render () {
+    return (
+      <nav className="navbar p-0 fixed-top d-flex flex-row">
+        <div className="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
+          <Link className="navbar-brand brand-logo-mini" to="/"><img src={require('../assets/images/logo-mini.svg').default} alt="logo" /></Link>
+        </div>
+        <div className="navbar-menu-wrapper flex-grow d-flex align-items-stretch">
+          <ul className="navbar-nav w-100">
+            <li className="nav-item w-100">
+              <form className="nav-link mt-2 mt-md-0 d-none d-lg-flex search">
+                <div className="input-group">
+                  <input className="form-control" type="search" id="earch-input" placeholder="Search items, Creators"/>
+                  <span className="input-group-append">
+                      <button className="btn ms-n5" type="button">
+                        <span className="mdi mdi-magnify"></span>
+                      </button>
+                  </span>
+              </div>
+              </form>
+            </li>
+          </ul>
+          <ul className="navbar-nav navbar-nav-right">
+            <button className="btn create-btn me-2">Create</button>
+            <button className="btn connect-btn">Connect</button>
+          </ul>
+          <button className="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" onClick={this.toggleOffcanvas}>
+            <span className="mdi mdi-format-line-spacing"></span>
+          </button>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
