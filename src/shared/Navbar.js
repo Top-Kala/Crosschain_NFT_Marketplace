42c1a80fcff05c4df3c6f00496c5fdd2254996a1
kevinharrisonfree77 <kevinharrisonfree77@gmail.com>
2021-12-25 00:58:59 +0530

    logo fix

--- src/shared/Navbar.js
+++ src/shared/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import lion2_icon from '../assets/images/lion2_icon.png';
 class Navbar extends Component {
   toggleOffcanvas() {
     document.querySelector('.sidebar-offcanvas').classList.toggle('active');
@@ -12,7 +12,7 @@ class Navbar extends Component {
     return (
       <nav className="navbar p-0 fixed-top d-flex flex-row">
         <div className="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
-          <Link className="navbar-brand brand-logo-mini" to="/"><img src={require('../assets/images/logo-mini.svg').default} alt="logo" /></Link>
+          <Link className="navbar-brand brand-logo-mini" to="/"><img src={lion2_icon} alt="logo" /></Link>
         </div>
         <div className="navbar-menu-wrapper flex-grow d-flex align-items-stretch">
           <ul className="navbar-nav w-100">
